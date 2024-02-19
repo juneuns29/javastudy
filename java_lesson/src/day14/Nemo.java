ce3a566e4725358f9e978e604530339aa4e1dda6
juneuns29 <54456501+juneuns29@users.noreply.github.com>
2024-02-19 17:44:09 +0900

    Add files via upload

---
+++ java_lesson/src/day14/Nemo.java
@@ -0,0 +1,50 @@
+package day14;
+
+public class Nemo implements Figure {
+	private int width, height;
+	private double area;
+	
+	public Nemo() {
+	}
+	public Nemo(int width, int height) { // 생성자 오버로딩
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public void setArea() {
+		area = width * height; // int ===> double 로 자동 형변환
+	}
+	
+	public void setArea(double area) { // setArea() 오버로딩
+		this.area = area;
+	}
+
+	@Override
+	public double getArea() {
+		return area;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	@Override
+	public String toString() {
+		return "■ ■ ■ 네모 :\n\t가로 : " + width + ", 세로 : " + height + ", 넓이 : " + area;
+	}
+
+	
+}
