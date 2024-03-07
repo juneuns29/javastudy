f36cbdaa014d4f4eae3b07a268b62dc2b040808a
juneuns29 <54456501+juneuns29@users.noreply.github.com>
2024-03-07 17:17:01 +0900

    Add files via upload

--- java_lesson/src/day26/Test01_JDBC.java
+++ java_lesson/src/day26/Test01_JDBC.java
@@ -26,44 +26,84 @@ public class Test01_JDBC {
 			// 이 함수는 예외를 전이하는 함수이므로
 			// 반드시 함수를 호출하는 곳에서 예외처리를 해줘야 한다.
 			
-			// 질의명령 꺼내오고
-			sql = getSQL(Test01_JDBC.SEL_MEMB_NAMES);
-//			System.out.println("*** SQL : " + sql);
+//			getNames();
 			
-			// 질의명령이 준비됬으므로 이 질의명령을 전달할 도구가 필요하다.
-			setStmt();
-			
-			// 전달 도구가 준비됬으므로 질의명령을 실어서 보내고
-			// 결과를 받으면 된다.
-			// 이때 결과는 JDBC가 SELECT 질의명령의 결과를 ResultSet 객체로 반환해준다.
-			
-			rs = stmt.executeQuery(sql);
-			// 질의명령의 결과는 rs에 담겨있고
-			// 이때 커서는(작업진행줄) BOF에 있다.
-			// 그래서 데이터를 꺼내려면 BOF 다음의 첫행으로 작업진행줄을 이동시켜야 한다.
-			// 그 명령이 rs.next() 이고 이 함수의 반환값은 
-			// 		true : 이동이 성공한 경우(EOF 가 아닌경우)
-			// 		false : 이동한 행이 EOF인 경우(꺼내올 데이터행으로 이동을 실패한 경우)
-			
-			// 조회결과가 몇행이 될지 알수 없으므로
-			while(rs.next()) {
-				// 작업진행줄을 한행 내리는 작업이 성공한 경우에만 반복을 진행
-				String name = rs.getString("name"); 
-				// 결과 행에서 name 컬럼의 데이터를 꺼내서 변수에 기억시킨다.
-				
-				// 출력
-				System.out.println(name);
-			}
+			replacePw(1000, "0000");
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				rs.close();
-				stmt.close();
+//				rs.close();
+//				stmt.close();
+				pstmt.close();
 				con.close();
 			} catch(Exception e) {}
 		}
 		
+	}
+	
+	// 회원번호와 비밀번호를 입력해주면 회원의 비밀번호를 변경해주는 함수
+	public void replacePw(int mno, String pw) throws SQLException {
+		// 질의명령 가져오고
+		sql = Test01_JDBC.getSQL(Test01_JDBC.EDIT_MEMB_PW);
+//		System.out.println("SQL : " + sql);
+		
+		// 명령 전달 도구 준비
+		pstmt = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		
+		// 그런데 아직 질의명령이 완성되지 않았으므로
+		// 질의명령을 완성해준다.
+		
+		pstmt.setString(1, pw);
+		pstmt.setInt(2, mno);
+		
+		// 질의명령이 완성됬으므로 질의명령을 보내고 결과를 받으면 된다.
+		// 	그런데 지금 보내는 질의명령은 update 질의명령이고
+		// 이런 질의명령을 보내고 결과를 받는 함수는
+		// execute(), executeUpdate(),..
+		
+		int row = pstmt.executeUpdate();
+		// 이 함수의 반환값은 변경된 행의 수를 반환해준다.
+		
+		if(row == 1) {
+			System.out.println("*** 비밀번호 변경에 성공했습니다. ***");
+		} else {
+			System.out.println("### 비밀번호 변경 작업에 문제가 있습니다. ###");
+		}
+		
+	}
+	
+	
+	// 회원이름 꺼내서 출력해주는 함수
+	public void getNames() throws SQLException {
+		// 질의명령 꺼내오고
+		sql = getSQL(Test01_JDBC.SEL_MEMB_NAMES);
+//		System.out.println("*** SQL : " + sql);
+		
+		// 질의명령이 준비됬으므로 이 질의명령을 전달할 도구가 필요하다.
+		setStmt();
+		
+		// 전달 도구가 준비됬으므로 질의명령을 실어서 보내고
+		// 결과를 받으면 된다.
+		// 이때 결과는 JDBC가 SELECT 질의명령의 결과를 ResultSet 객체로 반환해준다.
+		
+		rs = stmt.executeQuery(sql);
+		// 질의명령의 결과는 rs에 담겨있고
+		// 이때 커서는(작업진행줄) BOF에 있다.
+		// 그래서 데이터를 꺼내려면 BOF 다음의 첫행으로 작업진행줄을 이동시켜야 한다.
+		// 그 명령이 rs.next() 이고 이 함수의 반환값은 
+		// 		true : 이동이 성공한 경우(EOF 가 아닌경우)
+		// 		false : 이동한 행이 EOF인 경우(꺼내올 데이터행으로 이동을 실패한 경우)
+		
+		// 조회결과가 몇행이 될지 알수 없으므로
+		while(rs.next()) {
+			// 작업진행줄을 한행 내리는 작업이 성공한 경우에만 반복을 진행
+			String name = rs.getString("name"); 
+			// 결과 행에서 name 컬럼의 데이터를 꺼내서 변수에 기억시킨다.
+			
+			// 출력
+			System.out.println(name);
+		}
 	}
 	
 	public void setCon() throws ClassNotFoundException, SQLException {
@@ -95,7 +135,7 @@ public class Test01_JDBC {
 	}
 	
 	// 코드를 입력하고 실행하면 그 코드에 해당하는 질의명령 반환해주는 함수
-	public String getSQL(int code) {
+	public static String getSQL(int code) {
 		StringBuffer buff = new StringBuffer();
 		
 		switch(code) {
@@ -111,17 +151,17 @@ public class Test01_JDBC {
 			buff.append("UPDATE ");
 			buff.append("	member ");
 			buff.append("SET ");
-			buff.append("	ano = @@@ ");
+			buff.append("	ano = ? ");
 			buff.append("WHERE ");
-			buff.append("	mno = ### ");
+			buff.append("	mno = ? ");
 			break;
 		case EDIT_MEMB_PW:
 			buff.append("UPDATE ");
 			buff.append("	member ");
 			buff.append("SET ");
-			buff.append("	pw = '@@@' ");
+			buff.append("	pw = ? ");
 			buff.append("WHERE ");
-			buff.append("	mno = ### ");
+			buff.append("	mno = ? ");
 			break;
 		case ADD_MEMBER:
 			buff.append("INSERT INTO ");
